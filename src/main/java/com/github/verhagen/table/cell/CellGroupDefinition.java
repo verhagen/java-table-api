50c1b68ce0bca52d322c63d18f6bdd01dab6c3f5
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2015-12-12 11:42:14 +0100

    Adds CellGroupDefinition

--- src/main/java/com/github/verhagen/table/cell/CellGroupDefinition.java
+++ src/main/java/com/github/verhagen/table/cell/CellGroupDefinition.java
@@ -1,5 +1,47 @@
 package com.github.verhagen.table.cell;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CellGroupDefinition {
+	private final Map<String, CellDefinition> cellDefinitions; // = new HashMap<>();
+
+
+	public CellGroupDefinition(final Builder bldr) {
+		this.cellDefinitions = bldr.getCellDefinitions(); 
+	}
+
+
+	public Map<String, CellDefinition> getCellDefinitions() {
+		return Collections.unmodifiableMap(cellDefinitions);
+	}
+
+
+	public CellDefinition getCellDefinition(final String name) {
+		return cellDefinitions.get(name);
+	}
+
+
+	public static class Builder {
+		private Map<String, CellDefinition> cellDefinitions = new HashMap<>();
+		
+		
+		public CellGroupDefinition create() {
+			return new CellGroupDefinition(this);
+		}
+
+
+		public Map<String, CellDefinition> getCellDefinitions() {
+			return Collections.unmodifiableMap(cellDefinitions);
+		}
+
+
+		public Builder add(final CellDefinition cellDefinition) {
+			cellDefinitions.put(cellDefinition.getName(), cellDefinition);
+			return this;
+		}
+
+	}
 
 }
--- src/test/java/com/github/verhagen/table/cell/CellGroupDefinitionTest.java
+++ src/test/java/com/github/verhagen/table/cell/CellGroupDefinitionTest.java
@@ -1,5 +1,31 @@
 package com.github.verhagen.table.cell;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.testng.annotations.Test;
+
 public class CellGroupDefinitionTest {
+
+	@Test
+	public void createPerson() {
+		CellDefinition givenNameCellDef = new CellDefinition.Builder().setName("given-name").setType(String.class).create();
+		CellDefinition preposCellDef = new CellDefinition.Builder().setName("prepositions").setType(String.class).create();
+		CellDefinition surnameCellDef = new CellDefinition.Builder().setName("surname").setType(String.class).create();
+		CellDefinition birthDateCellDef = new CellDefinition.Builder().setName("birth-date").setType(Date.class).create();
+		
+		CellGroupDefinition cellGroupDef = new CellGroupDefinition.Builder()
+				.add(givenNameCellDef)
+				.add(preposCellDef)
+				.add(surnameCellDef)
+				.add(birthDateCellDef)
+				.create();
+		
+		CellDefinition cellDefinition = cellGroupDef.getCellDefinition("given-name");
+		assertEquals(cellDefinition, givenNameCellDef);
+		cellDefinition = cellGroupDef.getCellDefinition("birth-date");
+		assertEquals(cellDefinition, birthDateCellDef);
+	}
 
 }
